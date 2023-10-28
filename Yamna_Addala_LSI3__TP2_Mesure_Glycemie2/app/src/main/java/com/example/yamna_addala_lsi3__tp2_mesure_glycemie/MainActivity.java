83819f0553a67018470b52f01f09c8f88644d3ab
AddalaYamna <147881013+AddalaYamna@users.noreply.github.com>
2023-10-28 14:52:39 +0200

    Add files via upload

---
+++ Yamna_Addala_LSI3__TP2_Mesure_Glycemie2/app/src/main/java/com/example/yamna_addala_lsi3__tp2_mesure_glycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yamna_addala_lsi3__tp2_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
